cd5762f6f970ea8fe371770590065a0f1d2fa14e
elm-niconico <elmgameinfo@gmail.com>
2020-12-21 22:33:28 +0900

    プロジェクト ファイルを追加します。

---
+++ ultra-comment-viewer/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ultra_comment_viewer.src.viemodel;
+
+namespace ultra_comment_viewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var test = new ObservableCollection<CommentInfoModel>();
+            var comment = new CommentInfoModel() { Image=null,Comment="HELLO",UserName="TODO" };
+            CommentList.ItemsSource = test;
+            test.Add(comment);
+
+        }
+    }
+}
---
+++ ultra-comment-viewer/src/viemodel/CommentInfoModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace ultra_comment_viewer.src.viemodel
+{
+    public class CommentInfoModel
+    {
+        public BitmapImage Image { get; set; }
+        public string UserName   { get; set; }
+        public string Comment    { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Image} - {UserName} - {Comment}";
+        }
+    }
+}
